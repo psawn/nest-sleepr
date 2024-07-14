0dde64ae192b6f4dddf6d1dc473543e2b0fa5673
DoanDucBao <psawn0972055909@yahoo.com.vn>
2024-07-14 23:45:47 +0700

    init

---
+++ libs/common/src/database/database.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '../config/config.module';
+
+@Module({
+  // forRootAsync allow to inject dependencies -> get access to config service when init mongoose module
+  // useFactory tell nest how to actually create the mongoose module options
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+})
+export class DatabaseModule {}
---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { DatabaseModule } from '@app/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+// import { DatabaseModule } from '@app/common/database/database.module';
+
+@Module({
+  imports: [DatabaseModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
