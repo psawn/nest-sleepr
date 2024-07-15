42ae461b28212c063d9eb2afa00faf09ee018bc4
DoanDucBao <psawn0972055909@yahoo.com.vn>
2024-07-15 13:25:44 +0700

    update

---
+++ apps/reservations/src/reservations.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { DatabaseModule } from '@app/common';
+import { ReservationsService } from './reservations.service';
+import { ReservationsController } from './reservations.controller';
+import { ReservationsRepository } from './reservations.repository';
+import {
+  ReservationDocument,
+  ReservationSchema,
+} from './models/reservation.schema';
+
+@Module({
+  imports: [
+    DatabaseModule,
+    // forFeature is a static method that allows us to define which models we want to use in this module
+    DatabaseModule.forFeature([
+      { name: ReservationDocument.name, schema: ReservationSchema },
+    ]),
+  ],
+  controllers: [ReservationsController],
+  providers: [ReservationsService, ReservationsRepository],
+})
+export class ReservationsModule {}
